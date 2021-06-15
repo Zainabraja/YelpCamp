ba5d35f5510931a5b46b025247ea6a7a0de843ff
Zainab Raja <zainabraja53@gmail.com>
2021-06-16 00:16:36 +0530

    add start script

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize({
     replaceWith: '_'
 }))
-c
+
 const secret = process.env.SECRET || 'thisshouldbeabettersecret!'
 
 const store = new MongoDBStore({
@@ -159,8 +159,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
 
 
